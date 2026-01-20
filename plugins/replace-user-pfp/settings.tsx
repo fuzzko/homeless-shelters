be73a44cd6499716497e65ceb82a36f336cb8484
Komo <afiqquraisyzulkarnain@gmail.com>
2026-01-21 05:23:21 +0800

    push sync

--- plugins/replace-user-pfp/settings.tsx
+++ plugins/replace-user-pfp/settings.tsx
@@ -2,17 +2,38 @@ import { Settings } from "./types";
 
 const {
   plugin,
+  solid,
   ui: {
+    injectCss,
+    
     Text,
+    TextArea,
   },
 } = shelter;
 
 export const store = plugin.store as Settings;
 
+const TEXT_AREA_PLACEHOLDER = `https://... (or) data:image/...;base64,...`;
+
 export function settings() {
+  const [img, setImg] = solid.createSignal("");
+
+  injectCss(`
+    .in-same-line {
+      display: flex;
+    }
+
+    .in-same-line > * {
+      flex-grow: 1;
+    }
+  `);
+  
   return (
     <>
-      <Text>foo</Text>
+      <div class="in-same-line">
+        <img src={img()} />
+        <TextArea value={img()} onInput={setImg} resize-y={true} mono={true} placeholder={TEXT_AREA_PLACEHOLDER} />
+      </div>
     </>
   );
 }
